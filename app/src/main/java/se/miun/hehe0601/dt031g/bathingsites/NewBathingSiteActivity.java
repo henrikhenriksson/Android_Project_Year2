f8cdb71a1da36d4f6d97e7d8036972f2b1bd7f81
Henrik Henriksson <hehe0601@student.miun.se>
2020-05-12 13:22:26 +0200

    Writing logic for adding new bath site

--- app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/NewBathingSiteActivity.java
+++ app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/NewBathingSiteActivity.java
@@ -13,13 +13,23 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NewBathingSiteActivity extends AppCompatActivity {
+    private TextInputLayout textinputName;
+    private TextInputLayout textinputDescription;
+    private TextInputLayout textinputAddress;
+    private TextInputLayout textinputLatitude;
+    private TextInputLayout textinputLongitude;
+    private TextInputLayout textinputWaterTemp;
+    private TextInputLayout textinputWaterTempDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +38,29 @@ public class NewBathingSiteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        findViewsById();
+    }
+
+    private void findViewsById() {
+
+        textinputName = findViewById(R.id.text_input_bathing_site_name);
+        textinputDescription = findViewById(R.id.text_input_bathing_site_description);
+        textinputAddress = findViewById(R.id.text_input_bathing_site_address);
+        textinputLatitude = findViewById(R.id.text_input_bathing_site_latidude);
+        textinputLongitude = findViewById(R.id.text_input_bathing_site_longitude);
+       
+
+
+        // textinputLatitude.setFilter(new MinMaxFilter(-90, 90));
+
     }
 
 }
--- app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/NewBathingSiteActivityFragment.java
+++ app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/NewBathingSiteActivityFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 /*
  * FileName: NewBathingSiteActivity.java
  * Author: Henrik Henriksson (hehe0601)
@@ -20,15 +22,17 @@ import android.view.ViewGroup;
 
 public class NewBathingSiteActivityFragment extends Fragment {
 
+
     public NewBathingSiteActivityFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_bathing_site_activity, container, false);
     }
+
 }
