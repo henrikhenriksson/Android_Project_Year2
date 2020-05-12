e6b32deb96017c2991f8f425dfbd3a9d2a4b20d9
Henrik Henriksson <hehe0601@student.miun.se>
2020-05-12 10:17:57 +0200

    NewBathingSiteAcitivty created

---
+++ app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/NewBathingSiteActivityFragment.java
@@ -0,0 +1,28 @@
+package se.miun.hehe0601.dt031g.bathingsites;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewBathingSiteActivityFragment extends Fragment {
+
+    public NewBathingSiteActivityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_bathing_site_activity, container, false);
+    }
+}
