173a9f1fc297b7384028e90513edc4abe5328fea
Henrik Henriksson <hehe0601@student.miun.se>
2020-05-21 10:57:42 +0200

    Adding SettingsActivity

--- app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/MainActivity.java
+++ app/src/main/java/se/miun/hehe0601/dt031g/bathingsites/MainActivity.java
@@ -60,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            startActivity(i);
             return true;
         }
 
